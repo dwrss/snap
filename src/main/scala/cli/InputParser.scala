9dcb693d19b822047cc242c6600a14117d69114a
David Stephens <git@dwrs.org>
2020-06-18 22:30:45 +0100

    WIP CLI

---
+++ src/main/scala/cli/InputParser.scala
@@ -0,0 +1,28 @@
+package cli
+import atto._, Atto._
+import cats.syntax.either._
+
+trait Msg
+case class InvalidInput(message: String) extends Msg
+case class NumPlayers(message: Int) extends Msg
+case class MatchingMode(choice: MatchChoice) extends Msg
+
+trait MatchChoice
+case object SuitMatching extends MatchChoice
+case object ValueMatching extends MatchChoice
+case object BothMatching extends MatchChoice
+
+object InputParser {
+	val numPlayersParser = int
+	def parseNumPlayers(i: String): ParseResult[Int] = numPlayersParser.parseOnly(i)
+//		.either.getOrElse(InvalidInput("Invalid number of players"))
+
+	val suitParser: Parser[SuitMatching.type] = (stringCI("suit") | char('s')) >| SuitMatching
+	val valueParser: Parser[ValueMatching.type] = (stringCI("value") | char('v')) >| ValueMatching
+	val bothParser: Parser[BothMatching.type] = (stringCI("both") | char('b')) >| BothMatching
+	val matchStrategyParser: Parser[MatchChoice] = choice(suitParser, valueParser, bothParser)
+
+	def parseMatchStrategy(i: String): ParseResult[MatchChoice] = matchStrategyParser.parseOnly(i)
+//		.either
+//		.getOrElse(InvalidInput("Invalid "))
+}
---
+++ src/test/scala/CliSpec.scala
@@ -0,0 +1,101 @@
+import atto.ParseResult
+import cli.{BothMatching, InputParser, SuitMatching, ValueMatching}
+import org.scalatest.EitherValues
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.util.Random
+
+class CliSpec extends AnyWordSpec with Matchers with EitherValues {
+	def provided = afterWord("provided")
+	def is = afterWord("is")
+
+	"parseNumPlayers" when provided {
+		"a number" should {
+			"parse" in {
+				InputParser.parseNumPlayers("1") match {
+					case ParseResult.Done(remainder, result) =>
+						remainder shouldBe empty
+						result should be(1)
+				}
+			}
+		}
+		"a string" should {
+			"not parse" in {
+				val randomString = Random.nextString(5)
+				InputParser.parseNumPlayers(randomString) match {
+					case ParseResult.Fail(input, _, _) =>
+						input should be(randomString)
+				}
+			}
+		}
+	}
+	"parseMatchStrategy" when provided {
+		"a single character" should {
+			"parse one" which is {
+				"'s'" in {
+					InputParser.parseMatchStrategy("s") match {
+						case ParseResult.Done(remainder, result) =>
+							remainder shouldBe empty
+							result should be(SuitMatching)
+					}
+				}
+				"'v'" in {
+					InputParser.parseMatchStrategy("v") match {
+						case ParseResult.Done(remainder, result) =>
+							remainder shouldBe empty
+							result should be(ValueMatching)
+					}
+				}
+				"'b'" in {
+					InputParser.parseMatchStrategy("b") match {
+						case ParseResult.Done(remainder, result) =>
+							remainder shouldBe empty
+							result should be(BothMatching)
+					}
+				}
+			}
+			"not parse a character" which is {
+				"'t'" in {
+					InputParser.parseMatchStrategy("t") match {
+						case ParseResult.Fail(input, _, _) =>
+							input should be ("t")
+					}
+				}
+			}
+		}
+		"a word" should {
+			"parse one" which is {
+				"'suit'" in {
+					InputParser.parseMatchStrategy("suit") match {
+						case ParseResult.Done(remainder, result) =>
+							remainder shouldBe empty
+							result should be(SuitMatching)
+					}
+				}
+				"'value'" in {
+					InputParser.parseMatchStrategy("value") match {
+						case ParseResult.Done(remainder, result) =>
+							remainder shouldBe empty
+							result should be(ValueMatching)
+					}
+				}
+				"'both'" in {
+					InputParser.parseMatchStrategy("both") match {
+						case ParseResult.Done(remainder, result) =>
+							remainder shouldBe empty
+							result should be(BothMatching)
+					}
+				}
+			}
+			"not parse one" which is{
+				"'fooBar'" in {
+					InputParser.parseMatchStrategy("fooBar") match {
+						case ParseResult.Fail(input, _, _) =>
+							input should be ("fooBar")
+					}
+				}
+			}
+		}
+	}
+}
